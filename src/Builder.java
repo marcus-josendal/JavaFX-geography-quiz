1aedc6aa1ceabafecca2952bbe7ce50604ecf507
viking-v2 <31208951+viking-v2@users.noreply.github.com>
2018-03-04 21:33:31 +0100

    optimized imports

--- src/Builder.java
+++ src/Builder.java
@@ -1,13 +1,9 @@
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
